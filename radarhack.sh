7251fbc9af0605d6929883961d3a883869c83d99
Mztiger23 <60470329+Mztiger23@users.noreply.github.com>
2020-01-31 14:01:59 -0500

    Add files via upload

---
+++ radarhack.sh
@@ -0,0 +1,376 @@
+!/system/bin/bash
+#version 1.0
+
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet RADARHACK | lolcat 
+
+echo $bule "<============================================================>"
+echo                  "Tools    : Mz.tiger $white         " |lolcat
+echo            "Creadby  : Cyber Team Indonesia $white   " |lolcat
+echo          "Contact  : antoniusprio66@gmail.com $white " |lolcat
+echo $blue "<============================================================>"
+echo""
+echo "###################################################"
+      # CTRL + C
+echo "###################################################"
+
+trap ctrl_c INT
+ctrl_c() {
+
+if
+clear
+echo -e $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo -e $cyan"[#]> Thanks"
+echo ""
+echo -e $white"[#]> see you gaes :)..."
+exit
+
+# lagi=1
+while [ $lagi -lt 6 ];
+do
+echo ""
+echo -e $b "1.  sqlmap"
+echo -e "============================" | lolcat
+echo -e $r "2.  DDOS LITE"
+echo -e "============================" | lolcat
+echo -e $g "3.  RED_HAWK"
+echo -e "============================" | lolcat
+echo -e $c "4   sqlscan"
+echo -e "============================" | lolcat
+echo -e $r "5.  nikto "
+echo -e "============================" | lolcat
+echo -e $b "6.  B4J1N64Nv6"
+echo -e "============================" | lolcat
+echo -e $r "7.  DDOS TORJAN"
+echo -e "============================" | lolcat
+echo -e $g "8.  Mr.cakil"
+echo -e "============================" | lolcat
+echo -e $c "10  Metasploit"
+echo -e "============================" | lolcat
+echo -e $r "10. thc-hydra "
+echo -e "============================" | lolcat
+echo -e $r "11. Exit"
+echo ""
+echo -e "╭─0day" |lolcat
+read -p "╰─#" pil:"
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "Mz.tiger" | lolcat
+cd module
+php tp.php
+fi
+
+if[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -fstandard -d gay "Mz.tiger"
+cd module
+php t.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "Mz.tiger" | lolcat
+cd module
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet -f standard -d gay "Mz.tiger"
+cd module
+php hooq.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant -d gay "Mz.tiger"
+cd module
+php kfc.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -d gay "Mz.tiger"
+cd module
+php phd.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+php whiskas.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "Mz.tiger"
+cd module
+php zipay.php
+fi
+
+if [ $bro = 9 ] || [ $bro = 9 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+php key.php
+fi
+
+if [ $bro = 10 ] || [ $bro = 10 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+python2 SpamMail.py
+fi
+if [ $bro = 11 ] || [ $bro = 11 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+python2 whs.py
+fi
+
+if [ $bro = 12 ] || [ $bro = 12 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+python2 upf.py
+fi
+
+if [ $bro = 13 ] || [ $bro = 13 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+python2 create.py
+fi
+
+if [ $bro = 14 ] || [ $bro = 14 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+python2 dog.py
+fi
+
+if [ $bro = 15 ] || [ $bro = 15 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+python2 gam.py
+fi
+
+if [ $bro = 16 ] || [ $bro = 16 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+python2 anvima.py
+fi
+
+if [ $bro = 17 ] || [ $bro = 17 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+python2 htmd.py
+fi
+
+if [ $bro = 18 ] || [ $bro = 18 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+php autolike.php
+fi
+
+if [ $bro = 19 ] || [ $bro = 19 ]
+then
+clear
+toilet "Mz.tiger"
+cd module
+python2 auto_reaction.py
+fi
+
+if [ $bro = 20 ] || [ bro = 20 ]
+then
+clear
+figlet "Mz.tiger" | lolcat
+cd module
+python2 botkomen.py
+fi
+if [ $bro = 21 ] || [ $bro = 21  ]
+then
+clear
+toilet -f standard -F gay "Mz.tiger"
+apt update
+apt install git
+git clone https://github.com/sqlmapproject/sqlmap
+mv sqlmap $HOME
+cd $HOME/sqlmap
+read -p "masukan web target:" target
+python2 sqlmap.py -D target
+
+if [ $bro = 22 ] || [ $bro = 22 ]
+then
+toilet -f standard -f gay "Mz.tiger"
+apt update
+apt install git
+git clone git clone https://github.com/4L13199/LITEDDOS
+cd $HOME/LITEDDOS
+python2 liteDDOS.py
+read -p"masukan ip:target:" target
+python2 liteDDos.py -D target
+
+if [ $bro = 23 ] || [ $bro = 23 ]
+then
+toilet -f standard -f gay "Mz.tiger"
+apt update
+apt install git
+apt install php curl
+apt install python2
+apt install sh
+git clone https://github.com/Cvar1984/sqlscan.git
+cd $HOME/sqlscan
+chmod +x $PREFIX/bin/sqlscan
+sqlscan http://example.gov --scan
+sqlscan list_url.txt --scan
+
+if [ $bro = 24 ] || [ $bro = 24 ]
+then
+toilet -f standard -f gay "Mz.tiger"
+apt update
+apt install git
+apt install python2
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd $HOME/RED_HAWK
+php rhawk.php
+
+if [ $bro = 24 ] || [ $bro = 25 ]
+then
+toilet -f standard -f gay "Mz.tiger"
+apt update
+apt install git
+apt install python2
+apt install curl
+git clone https://github.com/sullo/nikto.git
+cd $HOME/nikto
+docker build -t sullo/nikto
+
+if [ $bro = 26 ] || [ $bro = 26 ]
+then
+toilet -f standard -f gay "Mz.tiger"
+apt update
+apt install curl
+apt install php
+apt install python2
+apt install figlet
+apt install toilet
+apt install git
+apt install sh
+git clone https://github.com/DarknessCyberTeam/B4J1N64Nv5.git
+cd $HOME/B4J1N64Nv5
+python2 B4J1N64N.py
+
+if [ $bro = 27 ] || [ $bro = 27 ]
+then
+toilet -f standard -f gay "Mz.tiger"
+apt update 
+apt install python
+apt install git
+git clone https://github.com/zlucifer/lucita_ddos
+cd $HOME/lucita_ddos
+chmod +x pukul.py
+python pukul.py
+
+if [ $bro = 28 ] || [ $bro = 28 ]
+then
+toilet -f standard -f gay "Mz.tiger"
+apt update
+apt install python2
+apt install git
+git clone https://github.com/Mrcakil/mrcakil
+cd $HOME/mrcakil
+chmod +x tools
+./tools
+
+if [ $bro = 29 ] || [ $bro = 29 ]
+then
+toilet -f standard -f gay "Mz.tiger"
+apt update
+apt update
+apt install git
+apt install ruby
+apt install curl
+apt install python2
+apt install figlet
+apt install toilet
+apt install php
+apt install wget 
+git clone https://github.com/rapid7/metasploit-framework.git
+cd $HOME/metasploit-fremework
+./metasploit.sh
+
+if [ $bro = 30 ] || [ $bro = 30 ]
+then
+toilet -f standard -f gay "Mz.tiger"
+apt update
+apt install git
+apt install php
+apt install python2
+apt install sh
+git clone https://github.com/Mr-R0N1Z/perusak.git
+cd $HOME/perusak
+sh perusak.sh
+
+echo "created by : Mz.tiger" | lolcat
+
+if [ $bro = 31 ]|| [ $bro = 31 ]
+*) echo "sorry, pilihan yang anda cari tidak ada"
+esac
+
+if [ $bro = 32 ] || [ $bro = 32 ]
+echo "\033[32;1m CYBER TEAM INDONESIA"
+sleep 1
+echo " We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal;)"
+sleep 1
+echo "Tidak ada kesuksesan tampa kegagalan"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+done
+exit
+fi
